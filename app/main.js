fdb701c8a9c17f47dd81cd8362c44ea5b529b41a
mahikero <federicoparassarmiento@yahoo.com>
2017-01-16 11:05:14 -0500

    up

--- app/main.js
+++ app/main.js
@@ -1,7 +1,7 @@
 "use strict";
 var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var core_1 = require('@angular/core');
+//import {enableProdMode} from '@angular/core';
 var app_module_1 = require('./app.module');
-core_1.enableProdMode();
+//enableProdMode();
 platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
 //# sourceMappingURL=main.js.map
