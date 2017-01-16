5025e26f513273e9b27203d7625df5d8f2309561
mahikero <federicoparassarmiento@yahoo.com>
2017-01-16 14:44:01 -0500

    up

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -11,7 +11,7 @@ import { HeroService } from './hero.service';
 })
 
 export class DashboardComponent implements OnInit {
-
+  
   heroes: Hero[] = [];
 
   constructor(private heroService: HeroService) { }
