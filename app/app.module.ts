3557c3326bb30c5cca64566c5288b3d60dd844a8
mahikero <federicoparassarmiento@yahoo.com>
2017-01-16 15:59:07 -0500

    up

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppRoutingModule }     from './app-routing.module';
+
 
 import { AppComponent }  from './app.component';
 import { DashboardComponent }   from './dashboard.component';
@@ -8,12 +12,12 @@ import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
 import { HeroService }         from './hero.service';
 
-import { AppRoutingModule }     from './app-routing.module';
 
 @NgModule({
   imports: [ 
     BrowserModule,
     FormsModule,
+    HttpModule,
     AppRoutingModule
   ],
   declarations: [ 
